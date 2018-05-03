2f4ae47c4c1d67b8400761283992d9709faeba31
Beeey <bs_magicer_064@yahoo.co.jp>
2018-05-03 17:24:00 +0900

    フォームを作成
    西暦を入力させる

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>和暦西暦変換</title>
+</head>
+<body>
+    <form action="action.php" method="get">
+        <label>西暦:<input type="text" name="AD" maxlength="4" value=""></label>
+        <input type="submit" value="送信する">
+    </form>
+</body>
+</html>
